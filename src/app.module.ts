2f6b63985176c6ab574386961ded48fd3b51498a
Aryan <aryan.tamboli@mitaoe.ac.in>
2026-01-18 15:33:49 +0530

    Phase 2: Setup database config and folder structure

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Only for development
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      logging: false,
+    }),
+  ],
+})
+export class AppModule {}
